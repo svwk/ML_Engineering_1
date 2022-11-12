c74edc0457bb7dd61b9e33d0d7049b3cce4b39cd
SavSV <sv.ework@gmail.com>
2022-11-12 18:10:10 +0300

    result_text fix

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from transformers import pipeline
 generator = pipeline("text-generation", "gpt2")
 source_text = input("Введите фразу для генерации нового текста: ")
 text_length = int(input("Введите длину текста: "))
-result_text=generator("I think", max_length=100)
+result_text = generator(source_text, max_length=text_length)
 print("Сгенерированный текст: ")
 print(result_text[0]['generated_text'])
