02b5bbd57e106fadc19c596c25c1581c12206371
SavSV <sv.ework@gmail.com>
2022-11-12 17:51:22 +0300

    input and print added

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 # practice implementation
 from transformers import pipeline
 
-generator = pipeline("text-generation")
-print(generator("I think", max_length=100))
+generator = pipeline("text-generation", "gpt2")
+source_text = input("Введите фразу для генерации нового текста: ")
+text_length = int(input("Введите длину текста: "))
+result_text=generator("I think", max_length=100)
+print("Сгенерированный текст: ")
+print(result_text[0]['generated_text'])
